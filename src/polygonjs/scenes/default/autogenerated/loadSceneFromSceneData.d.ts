07d65551b38306645a62e3f12a7b1f48587a706f
Guillaume Fradin <guillaumefradin@gmail.com>
2022-03-28 19:48:53 +0100

    update plugin and polygonjs to 1.1.246

--- src/polygonjs/scenes/default/autogenerated/loadSceneFromSceneData.d.ts
+++ src/polygonjs/scenes/default/autogenerated/loadSceneFromSceneData.d.ts
@@ -1,26 +1,24 @@
-import { ScenePlayerImporter } from '@polygonjs/polygonjs/dist/src/engine/io/player/Scene';
-import {configureScene} from '../PolyConfig';
 import {Poly} from '@polygonjs/polygonjs/dist/src/engine/Poly';
-import {AllExpressionsRegister} from '@polygonjs/polygonjs/dist/src/engine/poly/registers/expressions/All';
-import {configurePolygonjs} from '../PolyConfig';
-import {requiredImports_default} from './requiredImports';
-import { SceneJsonExporterData } from '@polygonjs/polygonjs/dist/src/engine/io/json/export/Scene';
-import { OnProgressCallback } from './loadSceneData';
-import { PolySceneWithNodeMap_default } from './PolySceneWithNodeMap';
-import { BaseViewerType } from '@polygonjs/polygonjs/dist/src/engine/viewers/_Base';
+import {SceneJsonExporterData} from '@polygonjs/polygonjs/dist/src/engine/io/json/export/Scene';
+import {OnProgressCallback} from './loadSceneData';
+import {PolySceneWithNodeMap_default} from './PolySceneWithNodeMap';
+import {BaseViewerType} from '@polygonjs/polygonjs/dist/src/engine/viewers/_Base';
 
 export interface loadSceneOptions {
 	sceneData: SceneJsonExporterData;
 	onProgress: OnProgressCallback;
+	autoPlay?: boolean;
 	domElement?: HTMLElement;
 	runRegister?: boolean;
+	assetsRoot?: string;
+	libsRootPrefix?: string;
 }
 interface LoadedData {
-	scene: PolySceneWithNodeMap_default,
-	viewer: BaseViewerType|undefined
+	scene: PolySceneWithNodeMap_default;
+	viewer: BaseViewerType | undefined;
 }
-export type LoadSceneMethod = (options: loadSceneOptions) => Promise<LoadedData|undefined>;
+export type LoadSceneMethod = (options: loadSceneOptions) => Promise<LoadedData | undefined>;
 
-const loadSceneFromSceneData_default: LoadSceneMethod;
+declare const loadSceneFromSceneData_default: LoadSceneMethod;
 
 export {Poly, loadSceneFromSceneData_default};
